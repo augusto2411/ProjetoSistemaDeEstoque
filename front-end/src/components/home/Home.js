3406aed0d1e6a41b8f565d3f6dce67a1dbeb9d49
Augusto <augustolacerda2411@gmail.com>
2026-06-22 12:34:12 -0300

    Ajustando o react

---
+++ front-end/src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../css/Home.module.css';
+
+function Home() {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.title}>Welcome to Our Website</h1>
+    </div>
+  );
+}
+
+export default Home;
